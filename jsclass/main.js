30c17961fb8e39c9cf2e1f9fbb735a0e3c67ff04
sisima <mas264@newschool.edu>
2017-10-11 19:02:19 -0400

    1

--- jsclass/main.js
+++ jsclass/main.js
@@ -1,4 +1,4 @@
 
 var images = ['ballo.jpg', 'bcare.jpg', 'candy.jpg','humans.jpg','box.jpg'];
-$('<img class="fade-in" src="image/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#random');
+$('<img class="fade-in" src="/image/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#random');
 
